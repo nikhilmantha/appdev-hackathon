1e72affefa3008500d15180841266a9cb1a43b73
EvanTaub <evanjtaubenfeld@gmail.com>
2025-11-26 15:56:50 -0500

    added middleware support to main.py (react port)

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,24 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
+from pydantic import BaseModel
+from pathlib import Path
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+origins = [
+    "add local host for react here (individual port)" # will figure out hosting later
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+#  root page
 @app.get("/")
 async def root():
     return {"message":"hello world"}
