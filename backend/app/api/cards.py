dcdc6eaeff80456d6ee7120cc59f7e790c00d51f
EvanTaub <evanjtaubenfeld@gmail.com>
2025-12-07 20:27:43 -0500

    created relational database between goals and users

---
+++ backend/app/api/cards.py
@@ -0,0 +1,172 @@
+# file to hold all tasks and card related functionality
+from fastapi import APIRouter, HTTPException, status
+from ..schemas.schemas import UserRegisterRequest, UserModel, UserLoginRequest  #shows how to import a schema
+from pymongo import AsyncMongoClient
+from dotenv import load_dotenv
+import os
+from bson import ObjectId
+from fastapi.responses import Response
+from datetime import date, datetime
+import random
+
+load_dotenv
+
+MONGO_URL = os.getenv("MONGO_URL")
+
+client = AsyncMongoClient(MONGO_URL)
+
+db = client.appDevHacktahon
+users_collection = db.get_collection("users")
+user_goals_collection = db.get_collection("user_goals")
+user_cards_collection = db.get_collection("user_cards")
+goal_templates_collection = db.get_collection("goal_templates")
+cards_collection = db.get_collection("cards")
+
+
+
+
+router = APIRouter(tags = ["cards"])
+
+# heloer method to convert ObjectIds into strings
+# specifically for the user case when 
+def serialize(doc):
+    doc["_id"] = str(doc["_id"])
+    doc["user_id"] = str(doc["user_id"])
+    doc["goal_id"] = str(doc["goal_id"])
+    return doc
+
+@router.post("/tasks/{user_id}",
+             response_description = "Create/See your goals",
+             )
+async def create_tasks(user_id : str):
+    # get the current date as a string
+    today_str = date.today().isoformat()
+
+    # count how many goals are already created
+    current_count = await user_goals_collection.count_documents({
+        "user_id" : ObjectId(user_id),
+        "assigned_for" : today_str
+    })
+
+
+    if current_count >= 3:
+        # goals are already created
+        goals = user_goals_collection.find({
+            "user_id" : ObjectId(user_id),
+            "assigned_for" : today_str
+        })
+
+        # convert object ids into jsonable type (String)
+        return [serialize(goal) async for goal in goals]
+    
+
+    # pick 3 goals (for demo only going to grab first 3)
+    picked_goals = goal_templates_collection.find().limit(3 - current_count)
+    templates = [template async for template in picked_goals]
+
+    new_user_goals = []
+    for template in templates:
+        new_user_goals.append({
+            "user_id" : ObjectId(user_id),
+            "goal_id" : template["_id"],
+            "status" : "active",
+            "reward_packs" : template["reward_packs"],
+            "assigned_for" : today_str,
+            "created_at": datetime.now(),
+            "completed_at" : None
+        })
+    
+    
+    result = await user_goals_collection.insert_many(new_user_goals)
+
+    for g, inserted_id in zip(new_user_goals, result.inserted_ids):
+        g["_id"] = inserted_id
+
+    return [serialize(g) for g in new_user_goals]
+
+
+@router.post("/tasks/{user_id}/goal/{user_goal_id}/complete",
+             response_description = "Endpoint for completing a goal")
+async def complete_goal(user_id : str, user_goal_id: str):
+    user_goal = await user_goals_collection.find_one({
+        "_id" : ObjectId(user_goal_id),
+        "user_id" : ObjectId(user_id),
+        "status" : "active"
+    })
+
+    # goal not found in db table
+    if not user_goal:
+        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND,
+                            detail = "Active goal not found")
+    
+
+    # update the relational table between users and goals
+    await user_goals_collection.update_one(
+        {"_id" : user_goal["_id"]},
+        {"$set": {"status" : "completed", "completed_at" : datetime.now()}}
+
+    )
+
+
+    # update the user's internally tracked metrics
+    await users_collection.update_one(
+        {"_id": ObjectId(user_id)},
+        {
+            "$inc": {
+                "completed_goals" : 1,
+                "packs_available" : user_goal.get("reward_packs", 1)
+            }
+        }
+    )
+
+
+    return {"message" : "Goal completed, packs awarded"}
+
+
+@router.post("/users/{user_id}/packs/open", 
+             response_description = "Open a pack associated with a specific server")
+async def open_pack(user_id : str):
+    user = await users_collection.find_one({"_id" : ObjectId(user_id)})
+
+    if not user:
+        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND,
+                            detail = "User not found")
+
+    if user.get("packs_available", 0) <= 0:
+        raise HTTPException(status_code = status.HTTP_400_BAD_REQUEST,
+                            detail = "No Packs Available")
+    
+    # decrement the pack count
+    await users_collection.update_one(
+        {"_id" : ObjectId(user_id)},
+        {"$inc" : {"packs_available" : -1}}
+    )
+
+    # fetch some cards (all for demo)
+    cards_cursor = cards_collection.find()
+    all_cards = [card async for card in cards_cursor]
+
+    # no cards in db
+    if len(all_cards) == 0:
+        raise HTTPException(status_code = status.HTTP_500_INTERNAL_SERVER_ERROR,
+                            detail = "No cards in database")
+    
+    pulled = random.sample(all_cards, k = 4) # pulls four random cards
+
+    # update user, card relationship
+    for card in pulled:
+        await user_cards_collection.update_one(
+            # search criteria
+            {"user_id" : ObjectId(user_id),
+             "card_id" : card["_id"]},
+
+            # changing the quantity
+             {"$inc" : {"quantity" : 1}},
+             #insert into db if it doesn't exist
+             upsert = True
+        )
+
+        return { "cards" : 
+                [ {"card_id" : str(card["id"]), "name" : card["name"] , "rarity" : card["rarity"]}  for card in pulled]}
+                
+
--- backend/app/api/user.py
+++ backend/app/api/user.py
@@ -1,8 +1,56 @@
 # file to hold endpoints involving a logged in user (goals and profile)
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException, status
+from ..schemas.schemas import UserRegisterRequest, UserModel, UserLoginRequest  #shows how to import a schema
+from pymongo import AsyncMongoClient
+from dotenv import load_dotenv
+import os
+from bson import ObjectId
+from fastapi.responses import Response
+load_dotenv
+
+MONGO_URL = os.getenv("MONGO_URL")
+
+
 
 router = APIRouter(tags = ["user"])
+client = AsyncMongoClient(MONGO_URL)
 
+db = client.appDevHacktahon
+users_collection = db.get_collection("users")
+user_goals_collection = db.get_collection("user_goals")
+user_cards_collection = db.get_collection("user_cards")
+
+
+# all of these routes assume you have a user account
+
+
+# INCOMPLETE
+# STILL NEEDS CARDS AND GOAL IMPLEMENTATION
+@router.get("/profile/{user_id}",
+             response_description = "Go to a specific user's profile",
+             response_model_by_alias = False,
+             response_model = dict)
+async def profile(user_id : str):
+
+    user_db = await users_collection.find_one({"_id" : ObjectId(user_id)})
+
+    if not user_db:
+        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND,
+                            detail = "No user with that ID exists")
+
+    cards_cursor = user_cards_collection.find({"user_id" : ObjectId(user_id)})
+    user_cards = [card async for card in cards_cursor]
+
+    # change ObjectIds into strings
+    for c in user_cards:
+        c["_id"] = str(c["_id"])
+        c["user_id"] = str(c["user_id"])
+        c["card_id"] = str(c["card_id"])
+    
+    # cast the userDB model into the UserModel
+    user = UserModel(**user_db)
+
+    return {"user" : user, "user_cards" : user_cards}
 
 
 
