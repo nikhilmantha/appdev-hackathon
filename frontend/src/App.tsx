f4be82b5191672b4f7a68a027fb3186ea3280f03
Nikhil <nikhil.k.mantha@gmail.com>
2025-12-08 00:54:14 -0500

    Cleaned upp App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,36 +10,6 @@ import Catalog from "./pages/Catalog"
 import Tasks from "./pages/Tasks"
 import Profile from "./pages/Profile"
 
-
-function Home() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
-}
-
 export default function App() {
   return (
     <BrowserRouter>
