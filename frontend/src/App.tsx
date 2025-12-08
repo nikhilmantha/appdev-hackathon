2cdf234ff5a819bd56bdb331badbd5197596aeef
Nikhil <nikhil.k.mantha@gmail.com>
2025-12-08 01:14:34 -0500

    Cleaned up App.tsx and App.css

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,4 @@
-import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
